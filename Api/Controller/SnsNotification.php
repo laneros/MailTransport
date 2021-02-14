fb05132343ecbb98317de090fc61110d63ac6ed4
Julian Munoz <juliand@laneros.com>
2021-02-14 17:34:30 -0500

    I decided to ditch support for AWS API and instead implement an endpoint to receive SNS bounce and complaint notifications.

---
+++ Api/Controller/SnsNotification.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Laneros\MailTransport\Api\Controller;
+
+use Laneros\MailTransport\AwsBounce\Parser;
+use XF\Api\Controller\AbstractController;
+
+class SnsNotification extends AbstractController
+{
+    public function actionPost()
+    {
+        $content = $this->request()->getInputRaw();
+
+        $message = json_decode($content);
+
+        if (empty($message->Type)) {
+            \XF::logError('Amazon SNS payload does not contains a type: ' . $content);
+            return $this->apiError('Amazon SNS payload does not contains a type', 'amazon_no_type');
+        }
+
+        switch ($message->Type) {
+            case 'SubscriptionConfirmation':
+                if (!empty($message->SubscribeURL)) {
+                    file_get_contents($message->SubscribeURL);
+                }
+
+                return $this->apiSuccess();
+                break;
+            case 'Notification':
+                if (empty($message->Message)) {
+                    \XF::logError('No message provided: ' . $content);
+                    return $this->apiError('No message provided', 'amazon_no_message');
+                }
+                break;
+            default:
+                \XF::logError('Unsupported notification message: ' . $content);
+                return $this->apiError('Unsupported notification message', 'amazon_no_notification_message');
+        }
+
+        $parser = $this->getParser();
+
+        $processor = $this->getProcessor($parser);
+
+        $processor->processMessage($content);
+
+
+        return $this->apiSuccess();
+    }
+
+    protected function getParser()
+    {
+        $options = $this->app->options();
+
+        $class = $this->app->extendClass('Laneros\MailTransport\AwsBounce\Parser');
+
+        return new $class(
+            $options->enableVerp ? $options->bounceEmailAddress : null,
+            $this->app->config('globalSalt')
+        );
+    }
+
+    protected function getProcessor(Parser $parser)
+    {
+        $class = $this->app->extendClass('XF\EmailBounce\Processor');
+
+        return new $class($this->app, $parser);
+    }
+
+    public function allowUnauthenticatedRequest($action)
+    {
+        return $action === 'post';
+    }
+}
---
+++ AwsBounce/Parser.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Laneros\MailTransport\AwsBounce;
+
+use Laminas\Mail\Storage\Message;
+use XF\EmailBounce\ParseResult;
+
+class Parser extends \XF\EmailBounce\Parser
+{
+    public function parseMessage($message)
+    {
+        $message = json_decode($message);
+
+        $message = json_decode($message->Message);
+
+        if (empty($message->notificationType)) {
+            throw new \InvalidArgumentException("No Amazon SNS notification type provided: " . $message);
+            return false;
+        }
+
+        $result = new ParseResult();
+
+        switch ($message->notificationType) {
+            case 'Bounce':
+                $result->messageType = ParseResult::TYPE_BOUNCE;
+
+                $recipient = reset($message->bounce->bouncedRecipients);
+                $result->remoteStatus = $recipient->status;
+                $result->remoteDiagnostics = $recipient->diagnosticCode;
+
+                break;
+
+            case 'Complaint':
+                $recipient = reset($message->mail->destination);
+                $result->messageType = ParseResult::TYPE_BOUNCE;
+
+                $result->remoteStatus = '5.1.1'; //We just want to treat this complaint as a hard bounce
+                $result->remoteDiagnostics = 'complaint';
+                break;
+
+            default:
+                throw new \InvalidArgumentException("Unsupported notification type");
+                return false;
+        }
+
+        $dt = new \DateTime($message->mail->timestamp);
+        $result->date = intval($dt->format('U'));
+
+        $result->textContent = null;
+        $result->originalContent = null;
+
+        $xToValidate = '';
+
+        foreach ($message->mail->headers as $header) {
+            if ($header->name == 'X-To-Validate') {
+                $xToValidate = $header->value;
+            }
+        }
+
+        if ($this->verpBase) {
+            if (preg_match('#([a-z0-9]+)\+([^\s]+)#i', $xToValidate, $match)) {
+                $email = $match[2];
+                $hmac = hash_hmac('md5', $email, $this->verpHmacKey);
+
+                $result->recipientTrusted = (substr($hmac, 0, strlen($match[1])) === $match[1]);
+                $result->recipient = $email;
+            }
+        } else {
+            // no VERP enabled, so we need to trust the recipient that we find
+            $result->recipientTrusted = true;
+            $result->recipient = $recipient->emailAddress ?? $recipient;
+        }
+
+        return $result;
+    }
+}
